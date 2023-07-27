aedf697c7a00f53f78ef0b193967814d6973e072
saied2035 <saied1998@gmail.com>
2023-07-27 12:25:54 +0300

    Create records controller & Set its routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :records, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy] do
+    resources :records, only: [:index, :show, :create, :update, :destroy]
+  end  
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
