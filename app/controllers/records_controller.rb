c3debba5f98a090bbced078a0aa0aa0c1a082764
saied2035 <saied1998@gmail.com>
2023-07-27 13:09:13 +0300

    mend
    Finish actions for record controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,40 +1,44 @@
 class RecordsController < ApplicationController
-	load_and_authorize_resource
-	def index
-		@records = Record.accessible_by(current_ability)
-		render json: @records
-	end
-	def show
-       render json: @record
-	end
-	def create
-		@user = User.find(params[:user_id])
-		@record = Record.new(record_params)
-		@record.user = @user
-		if @record.save
-		  render json: "Record has been created successfully", status: :ok
-		else
-		  render json: "Faild to save record into database. Please check your body request.", status: :unprocessable_entity	
-		end	
-	end
-	def update
-		if @food.update(record_params)
-		  render json: "Record has been created successfully", status: :ok
-		else
-		  render json: "Faild to update record. Please check your body request.", status: :unprocessable_entity
-		end  	
-	end
-	def destroy
-	if @user.destroy
+  load_and_authorize_resource
+  def index
+    @records = Record.accessible_by(current_ability)
+    render json: @records
+  end
+
+  def show
+    render json: @record
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @record = Record.new(record_params)
+    @record.user = @user
+    if @record.save
+      render json: 'Record has been created successfully', status: :ok
+    else
+      render json: 'Faild to save record into database. Please check your body request.', status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @record.update(record_params)
+      render json: 'Record has been created successfully', status: :ok
+    else
+      render json: 'Faild to update record. Please check your body request.', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @record.destroy
       render json: 'The record has been removed successfully.'
     else
       render json: 'Record id is wrong, or the record is already removed.'
     end
-	end
+  end
 
-	private
+  private
 
-	def record_params
-	  params.require(:record).permit(:time, :date, :distance)
-	end	
+  def record_params
+    params.require(:record).permit(:time, :date, :distance)
+  end
 end
